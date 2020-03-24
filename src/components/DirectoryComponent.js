3ce87d169981ec624663d16c4531f6e379402214
jmil191 <jda.milton@gmail.com>
2020-03-23 20:09:52 -0500

    Nucamp Site adding basic react elements

---
+++ src/components/DirectoryComponent.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import  CampsiteInfoComponent from './CampsiteInfoComponent';
+
+class Directory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedCampsite: null
+        };
+    }
+
+    onCampsiteSelect(campsite) {
+        console.log(campsite)
+        this.setState({selectedCampsite: campsite});
+    }
+ 
+
+    render() {
+        const directory = this.props.campsites.map(campsite => {
+            return (
+                <div key={campsite.id} className="col-md-5 m-1">
+                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{campsite.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    {directory}
+                </div>
+                <CampsiteInfoComponent campsite ={this.state.selectedCampsite} />
+    
+            </div>
+        );
+    }
+}
+
+export default Directory;
